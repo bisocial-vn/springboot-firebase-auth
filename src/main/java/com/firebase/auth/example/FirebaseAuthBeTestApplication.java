ebd23ee533755f7450b0f62b3692e3f6f1f77336
Mr.Bi <minh.mrbi@gmail.com>
2021-04-01 21:33:08 +0700

    initial

---
+++ src/main/java/com/firebase/auth/example/FirebaseAuthBeTestApplication.java
@@ -0,0 +1,13 @@
+package com.firebase.auth.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirebaseAuthBeTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirebaseAuthBeTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/firebase/auth/example/FirebaseAuthBeTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.firebase.auth.example;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirebaseAuthBeTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
