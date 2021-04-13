bb990ac4402f3d11d5f237a724c7d2b02da466d1
MrBi <minh.mrbi@gmail.com>
2021-04-13 17:52:47 +0700

    update firebase initial fallback with default

--- src/main/java/com/firebase/auth/example/configuration/FirebaseConfiguration.java
+++ src/main/java/com/firebase/auth/example/configuration/FirebaseConfiguration.java
@@ -1,33 +1,47 @@
 package com.firebase.auth.example.configuration;
 
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-
-import javax.annotation.PostConstruct;
+import java.io.IOException;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import com.google.auth.oauth2.GoogleCredentials;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
 
+import lombok.extern.slf4j.Slf4j;
+
 @Configuration
+@Slf4j
 public class FirebaseConfiguration {
 
-	@Value("${app.firebase.credential.path}")
+	@Value("${app.firebase.credential.path:/test.json}")
 	private String firebaseCredentialPath;
 
-	@PostConstruct
-	public void initial() throws Exception {
+	@Bean
+	public FirebaseApp initial() {
+		GoogleCredentials googleCredentials = null;
 		try {
 			FileInputStream firebaseCredentialFileInputStream = new FileInputStream(firebaseCredentialPath);
-			FirebaseOptions firebaseOptions = FirebaseOptions.builder()
-					.setCredentials(GoogleCredentials.fromStream(firebaseCredentialFileInputStream)).build();
-			FirebaseApp.initializeApp(firebaseOptions);
-		} catch (FileNotFoundException ex) {
-			throw new Exception("Initial firebase fail. " + ex.getMessage());
+			googleCredentials = GoogleCredentials.fromStream(firebaseCredentialFileInputStream);
+		} catch (IOException ex) {
+			try {
+				log.info("Initial firebase with default. firebaseCredentialPath: {}", firebaseCredentialPath);
+				googleCredentials = GoogleCredentials.getApplicationDefault();
+			} catch (IOException ex1) {
+				throw new RuntimeException("Fail to get google credential for firebase app.", ex);
+			}
 		}
+		FirebaseOptions firebaseOptions = FirebaseOptions.builder().setCredentials(googleCredentials).build();
+		FirebaseApp firebaseAppInstance = FirebaseApp.initializeApp(firebaseOptions);
+
+		log.info("Initial Firebase app success.\n\tApp name: {}", firebaseAppInstance.getName());
+		log.info("Firebase Databasr URL: {}", firebaseAppInstance.getOptions().getDatabaseUrl());
+		log.info("Firebase ProjectID: {}", firebaseAppInstance.getOptions().getProjectId());
+		log.info("Firebase Firestore: {}", firebaseAppInstance.getOptions().getStorageBucket());
+		return firebaseAppInstance;
 	}
 
 }
